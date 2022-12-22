c6fcbfa4610f524e084a5f40f70fb64828ea0386
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-12-22 22:14:31 +0400

    remove unused services file

--- src/services/web3.service.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-import { CONTRACT_ADDRESS, CONTRACT_INTERFACE } from "../constants";
-
-export const connectToWavePortalContract = (signer: ethers.Signer | ethers.providers.Provider | undefined) =>
-  new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_INTERFACE, signer);
